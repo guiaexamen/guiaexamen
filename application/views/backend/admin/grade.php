0b2ffa35c6cdfd3f8abe5a51f1e49eb41de16540
Osvaldo <dobalosvaldo@gmail.com>
2017-11-27 21:40:08 -0600

    Se creo el Primer commit

---
+++ application/views/backend/admin/grade.php
@@ -0,0 +1,17 @@
+<style type="text/css">
+<!--
+.style2 {color: #990000}
+.style3 {font-size: 18px}
+-->
+</style>
+<p align="center" class="style3"><strong>Disabled in Basic Version</strong></p>
+<p align="center" class="style3"><strong><a href="www.optimumlinkup.com.ng" target="_blank" class="style2"><u>Upgrade</u></a> to full version for just 30$ only. <u>100% Source Code</u> <a href="www.optimumlinkup.com.ng" target="_blank"><u><span class="style2">www.Optimum Linkup.com.ng</span> </u></a></strong></p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><table>
+<tr>
+  <td>&nbsp;</td>
+</tr><tr>
+  <td>&nbsp;</td>
+</tr>
+</table>
+</form>
+<p><img src="uploads/images/admin/examgrades_admin.PNG" width="1045" height="504" /></p>
---
+++ application/views/backend/footer.php
@@ -0,0 +1,4 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2016 optimumlinkup.com.ng <strong> School Manager</strong>. 
+    Developed by Optimum Linkup </footer>
